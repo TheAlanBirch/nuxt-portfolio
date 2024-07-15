b16f06231b54a3b4e43996a5eb894ccaaafefcf9
Alex Brown <kemikalstealth@icloud.com>
2024-07-14 21:31:55 -0500

    Sections 1-4 Complete!

---
+++ app/layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink
+          to="/"
+          class="text-xl font-semibold p-2 hover:bg-gray-200 rounded-md"
+        >
+          Alex Brown
+        </NuxtLink>
+      </div>
+
+      <Menu />
+    </header>
+
+    <main class="p-2 mt-10">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  titleTemplate: "%s - Alex Brown",
+  link: [
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com",
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Roboto&display=swap",
+      crossorigin: "",
+    },
+  ],
+});
+</script>
+
+<style>
+body {
+  font-family: "Roboto", sans-serif;
+}
+</style>
---
+++ app/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This page is about me</div>
+  <!-- <div>
+    <button @click="enableCustomLayout">Update layout</button>
+  </div> -->
+</template>
+
+<script lang="ts" setup>
+useHead({
+  title: "About",
+});
+</script>
+
+<style></style>
